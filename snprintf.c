67c126d77227905b5d114ebb39c53a026dd52e9b
wtwzxl <hustwtw@hust.edu.cn>
2017-04-30 21:22:20 +0800

    Update and rename snprintf.txt to snprintf.c

---
+++ snprintf.c
@@ -0,0 +1,30 @@
+
+
+/**
+int snprintf(char *restrict buf,  size_t n,  const char * restrict  format, ...);
+函数说明:最多从源串中拷贝n－1个字符到目标串中，然后再在后面加一个0。所以如果目标串的大小为n 的话，将不会溢出。
+函数返回值:若成功则返回欲写入的字符串长度，若出错则返回负值。
+http://www.cplusplus.com/reference/cstdio/snprintf/
+
+*/
+/* snprintf example */
+#include <stdio.h>
+/*在vs2012 下面要作如下定义*/
+#if _MSC_VER
+#define snprintf _snprintf_s
+#endif
+int main ()
+{
+  char buffer [100];
+  int cx;
+
+  cx = snprintf ( buffer, 100, "The half of %d is %d", 60, 60/2 );
+
+  if (cx>=0 && cx<100)      // check returned value
+
+    snprintf ( buffer+cx, 100-cx, ", and the half of that is %d.", 60/2/2 );
+
+  puts (buffer);
+
+  return 0;
+}
